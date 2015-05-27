a322419fa2786b3e388d5f1fe407428b615f7ddf
Kensuke Nagae <kyanny@gmail.com>
2015-05-27 20:08:10 +0900

    Extract actual middleware implementation and spec files from API service

--- lib/real_ip.rb
+++ lib/real_ip.rb
@@ -1,5 +1,21 @@
-require "real_ip/version"
+# http://hemal.kuntawa.la/post/3958107290/client-ip-addresses-on-heroku
+class RealIp
+  def initialize(app)
+    @app = app
+  end
 
-module RealIp
-  # Your code goes here...
+  def call(env)
+    set_remote_addr env
+    @app.call env
+  end
+
+  def set_remote_addr(env)
+    fwd = env['HTTP_X_FORWARDED_FOR']
+    if fwd
+      ip = fwd.strip.split(/[,\s]+/)[0] # stolen from Rack::Request#split_ip_addresses
+    else
+      ip = env['HTTP_X_REAL_IP'] || env['REMOTE_ADDR']
+    end
+    env['QUIPPER_REMOTE_ADDR'] = ip
+  end
 end
--- spec/real_ip_spec.rb
+++ spec/real_ip_spec.rb
@@ -1,11 +1,48 @@
 require 'spec_helper'
 
 describe RealIp do
-  it 'has a version number' do
-    expect(RealIp::VERSION).not_to be nil
+  let(:app) { ->(env) {  } }
+  let(:mid) { RealIp.new app }
+
+  describe '#set_remote_addr' do
+    context 'have HTTP_X_FORWARDED_FOR' do
+      it 'set first ip addr to QUIPPER_REMOTE_ADDR' do
+        env = { 'HTTP_X_FORWARDED_FOR' => '9.9.9.9, 3.3.3.3' }
+        mid.set_remote_addr env
+        expect(env['QUIPPER_REMOTE_ADDR']).to eq '9.9.9.9'
+      end
+
+      it 'ignore other keys' do
+        env = { 'HTTP_X_FORWARDED_FOR' => '9.9.9.9', 'REMOTE_ADDR' => '3.3.3.3' }
+        mid.set_remote_addr env
+        expect(env['QUIPPER_REMOTE_ADDR']).to eq '9.9.9.9'
+      end
+    end
+
+    it 'set HTTP_X_REAL_IP or REMOTE_ADDR' do
+      env = { 'HTTP_X_REAL_IP' => '9.9.9.9', 'REMOTE_ADDR' => '3.3.3.3' }
+      mid.set_remote_addr env
+      expect(env['QUIPPER_REMOTE_ADDR']).to eq '9.9.9.9'
+    end
+
+    it 'set REMOTE_ADDR' do
+      env = { 'REMOTE_ADDR' => '3.3.3.3' }
+      mid.set_remote_addr env
+      expect(env['QUIPPER_REMOTE_ADDR']).to eq '3.3.3.3'
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#call' do
+    let(:env) { { 'REMOTE_ADDR' => '9.9.9.9' } }
+
+    it 'invoke set_remote_addr' do
+      expect(mid).to receive(:set_remote_addr).with(env)
+      mid.call env
+    end
+
+    it 'invoke app.call' do
+      expect(app).to receive(:call).with('QUIPPER_REMOTE_ADDR' => '9.9.9.9', 'REMOTE_ADDR' => '9.9.9.9')
+      mid.call env
+    end
   end
 end
